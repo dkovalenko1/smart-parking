c6ff9977e59f5c5379432c82e07c68c5805b50be
dkovalenko1 <dkovalenko1@kse.org.ua>
2026-05-13 18:20:36 +0300

    feat: add database pool connection, ping and dependency update

--- services/spot/cmd/main.go
+++ services/spot/cmd/main.go
@@ -1,12 +1,15 @@
 package main
 
 import (
+	"context"
 	"errors"
 	"os"
+	"time"
 
 	"github.com/golang-migrate/migrate/v4"
 	_ "github.com/golang-migrate/migrate/v4/database/postgres"
 	_ "github.com/golang-migrate/migrate/v4/source/file"
+	"github.com/jackc/pgx/v5/pgxpool"
 	logger "github.com/sirupsen/logrus"
 )
 
@@ -16,10 +19,23 @@ func main() {
 	dbURL := mustEnv("DATABASE_URL")
 
 	if err := runMigrations(dbURL); err != nil {
-		logger.Error("migrations failed: ", err)
-		os.Exit(1)
+		logger.Fatal("migrations failed: ", err)
 	}
 
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	pool, err := pgxpool.New(ctx, dbURL)
+	if err != nil {
+		logger.Fatal(err)
+	}
+	defer pool.Close()
+
+	err = pool.Ping(ctx)
+	if err != nil {
+		logger.Fatal("database ping failed: ", err)
+	}
+	logger.Info("database ping succeeded")
 }
 
 func runMigrations(dbURL string) error {
@@ -40,8 +56,7 @@ func runMigrations(dbURL string) error {
 func mustEnv(key string) string {
 	v := os.Getenv(key)
 	if v == "" {
-		logger.Error("missing required env variable: ", key)
-		os.Exit(1)
+		logger.Fatal("missing required env variable: ", key)
 	}
 	return v
 }
